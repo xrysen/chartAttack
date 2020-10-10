0be864491332c49a9ffa8628c622e3e40cca1a1e
Sean Oyler <sean.oyler@gmail.com>
2020-10-10 19:52:01 +0000

    Added setter methods to chartAttack class

--- js/chartAttack.js
+++ js/chartAttack.js
@@ -15,6 +15,34 @@ const chartAttack = {
 
     addLabel: function(label) {
       this.xAxisLabel.push(label);
+    },
+
+    setTitle: function(text) {
+      this.title = text;
+    },
+
+    setBarColour: function(colour) {
+      this.barColour = colour;
+    },
+
+    setWidth: function(barWidth) {
+      this.width = barWidth;
+    },
+
+    setHeight: function(barHeight) {
+      this.height = barHeight;
+    },
+
+    setTitleFontSize = function(size) {
+      this.titleFontSize = size;
+    }.
+
+    setTitleFontColour = function(colour) {
+      this.titleFontColour = colour;
+    }
+
+    drawBarChart: function(data, options, element) {
+
     }
 };
 
